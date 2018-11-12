1c6ef976ac5935e3ad3186c0875af1d0c43a8b00
pooria50 <pooria.maleki89@gmail.com>
2018-11-12 16:53:54 +0330

    committ

--- app/src/main/java/com/example/pooria/mygallery/MainActivity.java
+++ app/src/main/java/com/example/pooria/mygallery/MainActivity.java
@@ -20,8 +20,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, MainSendPostActivity.class);
         startActivity(intent);
         Toast.makeText(this, "Chekced", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "Chekced", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "Chekced", Toast.LENGTH_SHORT).show();
 
         //test
 
